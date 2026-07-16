99ae3f8845024142eac497ae10994345a19a06a9
⌯ 𝐁 𝐀 𝐁 𝐔 𝐋 ┇【🇮🇳】  <babulxqueen82@gmail.com>
2026-07-16 17:46:04 +0530

    Update default user name and watermark text

--- ShiviMusic/utils/thumbnails.py
+++ ShiviMusic/utils/thumbnails.py
@@ -259,7 +259,7 @@ def _truncate(draw, text, font, max_w):
     return text + "…"
 
 
-async def get_thumb(videoid: str, user_name: str = "QueenUser") -> str:
+async def get_thumb(videoid: str, user_name: str = "kirtiUser") -> str:
     output = f"cache/{videoid}.png"
     cache  = f"cache/thumb{videoid}.jpg"
     os.makedirs("cache", exist_ok=True)
@@ -532,7 +532,7 @@ async def get_thumb(videoid: str, user_name: str = "QueenUser") -> str:
         fill=c_base,
         anchor="lm"
     )
-    draw.text((1255, 45), "Dev | BABUL OP",                                          font=f_wm,  fill=TEXT_WHITE, anchor="rd")
+    draw.text((1255, 45), "Dev | RUDRA",                                          font=f_wm,  fill=TEXT_WHITE, anchor="rd")
 
     base.save(output, "PNG", optimize=True)
 
